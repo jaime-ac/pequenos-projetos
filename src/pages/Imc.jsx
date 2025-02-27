472bb323c4fc13f7893c863bfd541bd51a494d5a
jaime-ac <jaimeantonioca97@gmail.com>
2025-02-27 14:27:08 -0300

    update do exercício do cálculo de imc

--- src/pages/Imc.jsx
+++ src/pages/Imc.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react"
 
 
-function Imc() {
+function Imc({imagem, result}) {
+
     const [peso, setPeso] = useState();
     const [altura, setAltura] = useState();
+
+    function calcularImc(){
+
+        if (parseFloat(peso) === '' || parseFloat(altura) === ''){
+
+            alert("Atenção você precisa preencher todos os campos antes de prosseguri com o cálculo!")
+
+        }else{
+
+            const imc = (peso / (altura * altura)).toFixed(1)
+            alert(imc)
+        }
+    }
+
   return (
+
     <div className="container__imc">
 
         <div className="imc__header">
 
-            <label htmlFor="" className="texto__descricao">Desenvolva uma calculadora de Índice de Massa Corporal IMC que receba peso e altura</label>
-            <label htmlFor="" className="texto__descricao">e retorne o IMC e a classificação ex: abaixo do peso, normal, sobrepeso.</label>
-            <img src="/public/imc.png" alt="" className="gif__imc"/>
+            <div className="header__elements">
+
+                <label htmlFor="" className="texto__descricao">Desenvolva uma calculadora de Índice de Massa Corporal IMC que receba peso e altura</label>
+                <label htmlFor="" className="texto__descricao">e retorne o IMC e a classificação ex: abaixo do peso, normal, sobrepeso.</label>
+                <img src="/public/imc.png" alt="" className="gif__imc"/>
+
+            </div>
 
         </div>
 
@@ -22,7 +42,7 @@ function Imc() {
                 value={peso}
                 onChange={(event) => setPeso(event.target.value)}
                 className="imc__entrada--dados"
-                placeholder="digite o seu peso aqui" 
+                placeholder="digite o seu peso aqui em kilos (ex.: 74 ou 74.50)" 
             />
 
             <input 
@@ -30,14 +50,26 @@ function Imc() {
                 value={altura}
                 onChange={(event) => setAltura(event.target.value)}
                 className="imc__entrada--dados"
-                placeholder="digite a sua altura aqui"
+                placeholder="digite a sua altura aqui em metros (ex.: 1.80)"
             />
             
-            <button className="imc__calcular">Calcular</button>
+            <button className="imc__calcular" onClick={calcularImc}>Calcular</button>
 
         </div>
 
         <div className="imc__resultado">
+
+            <div className="imagem__resultado">
+
+                <img src={imagem} alt="" className="header__resultado--imagem"/>
+
+            </div>
+
+            <div className="texto__resultado">
+
+                {result}
+
+            </div>
 
         </div>
       
